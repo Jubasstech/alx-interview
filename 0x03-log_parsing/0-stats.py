737edcede1f7c31992839cf10697663f9a61ce2c
Emediong ‘Bendito’ Francis <emediongfrancis@gmail.com>
2022-04-29 02:59:17 +0100

    Parse logs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,7 +15,7 @@ def printStatus(dic, size):
 
 # sourcery skip: use-contextlib-suppress
 statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
+               "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
